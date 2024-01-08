f9930c1fe2e4357dd247c39b0fba2e02c6570222
rmuti56 <rmuti56@hotmail.com>
2024-01-08 16:48:36 +0700

    first commit

---
+++ create-eav-via-declarative.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Vendor\Module\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+
+/**
+ * Class ApplyNewAttribute
+ * @package Vendor\Module\Setup\Patch\Data
+ */
+class ApplyNewAttribute implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+    /**
+     * ApplyNewAttribute constructor.
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute
+            \Magento\Catalog\Model\Product::ENTITY,
+            // add your attribute here
+        ;
+        $setup->endSetup();
+    }
+}
